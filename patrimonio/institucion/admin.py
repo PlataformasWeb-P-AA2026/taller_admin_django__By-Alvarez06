3c9d3bb0937cdab0067f5fe79e0390f70ff63c2d
By-Alvarez06 <bvalvarez1.1@gmail.com>
2026-06-16 12:12:05 -0500

    update: Creacion de vistas y consultas en admin

--- patrimonio/institucion/admin.py
+++ patrimonio/institucion/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
+from institucion.models import *
+
+
 # Register your models here.
+class MuseoAdmin(admin.ModelAdmin):
+    # listado de atributos que se mostrará
+    # por cada registro
+    # se deja de usar la representación (str)
+    # de la clase
+    list_display = ("nombre", "ciudad", "anio_fundacion")
+    search_fields = ("nombre", "ciudad")
+
+
+admin.site.register(Museo, MuseoAdmin)
+admin.site.register(GuiaMuseo)
+admin.site.register(Exhibicion)
